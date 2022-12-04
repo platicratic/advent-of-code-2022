729b87196a20415fad35a4940fb517296009d987
cstamatin <cristian.stamatin@cloudflight.io>
2022-12-04 19:44:34 +0200

    Day 3, add simpler solution

--- day03/main.dart
+++ day03/main.dart
@@ -4,10 +4,10 @@ void partOne(final List<String> rucksacks) {
   int sum = 0;
   for (String rucksack in rucksacks) {
     Set<String> S = {};
-    for (int i = 0; i < rucksack.length ~/ 2; i ++) {
+    for (int i = 0; i < rucksack.length ~/ 2; i++) {
       S.add(rucksack[i]);
     }
-    for (int i = rucksack.length ~/ 2; i < rucksack.length; i ++) {
+    for (int i = rucksack.length ~/ 2; i < rucksack.length; i++) {
       if (S.contains(rucksack[i])) {
         sum += M[rucksack[i]]!;
         break;
@@ -21,20 +21,37 @@ void partTwo(final List<String> rucksacks) {
   int sum = 0;
   for (int i = 0; i < rucksacks.length; i += 3) {
     Map<String, int> H = {};
-    for (int j = 0; j < 3; j ++) {
-      for (int k = 0; k < rucksacks[i + j].length; k ++) {
-        if ((H[rucksacks[i + j][k]] != null && H[rucksacks[i + j][k]] == j) || (H[rucksacks[i + j][k]] == null && j == 0)) {
+    for (int j = 0; j < 3; j++) {
+      for (int k = 0; k < rucksacks[i + j].length; k++) {
+        if ((H[rucksacks[i + j][k]] != null && H[rucksacks[i + j][k]] == j) ||
+            (H[rucksacks[i + j][k]] == null && j == 0)) {
           H[rucksacks[i + j][k]] = j + 1;
         }
       }
     }
-    sum += M[H.entries.singleWhere((MapEntry<String, int> item) => item.value == 3).key]!;
+    sum += M[H.entries
+        .singleWhere((MapEntry<String, int> item) => item.value == 3)
+        .key]!;
+  }
+  print(sum);
+}
+
+void partTwoSimpler(final List<String> rucksacks) {
+  int sum = 0;
+  for (int i = 0; i < rucksacks.length; i += 3) {
+    for (int j = 0; j < rucksacks[i].length; j++) {
+      if (rucksacks[i + 1].contains(RegExp(rucksacks[i][j])) &&
+          rucksacks[i + 2].contains(RegExp(rucksacks[i][j]))) {
+        sum += M[rucksacks[i][j]]!;
+        break;
+      }
+    }
   }
   print(sum);
 }
 
 void createMap() {
-  for (int i = 0; i < 26; i ++) {
+  for (int i = 0; i < 26; i++) {
     M[String.fromCharCode('a'.codeUnitAt(0) + i)] = i + 1;
     M[String.fromCharCode('A'.codeUnitAt(0) + i)] = i + 27;
   }
@@ -47,4 +64,5 @@ void main() {
   createMap();
   partOne(rucksacks);
   partTwo(rucksacks);
+  partTwoSimpler(rucksacks);
 }
